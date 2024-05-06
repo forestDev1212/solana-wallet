ce201946c03356145e14ed91c525681a37e7c7e6
forestDev1212 <alexeidorman@gmail.com>
2024-05-06 15:41:35 +0800

    update: changed to modal

--- src/pages/WalletModal.tsx
+++ src/pages/WalletModal.tsx
@@ -2,23 +2,27 @@ import { Button, Heading, VStack, useStatStyles } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import React, { useState } from "react";
 import { useDisclosure, Modal, ModalBody, ModalOverlay, ModalHeader, ModalCloseButton, ModalFooter, ModalContent } from "@chakra-ui/react";
-import WalletModal from "./WalletModal";
 
 const Wallets = dynamic(() => import("../components/Wallets"), { ssr: false });
 
-const IndexPage: React.FC = () => {
-  const [modalState, setModalState] = useState(false)
-  const showModal = () => {
-    setModalState(true)
-  }
+const WalletModal: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
-    <VStack gap={8} mt={16} >
-      <WalletModal />
-    </VStack>
+      {/* <Heading>Connect Solana Wallet </Heading> */}
+      <Button onClick={onOpen}>Connect Wallet</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Select Wallet</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Wallets />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
 
   );
 }
-export default IndexPage
+export default WalletModal
